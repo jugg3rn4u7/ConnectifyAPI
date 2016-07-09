4c5a11381de0da76a215f87380538cc24afe3041
jugg3rn4u7 <shriku4u27@gmail.com>
2016-07-09 17:32:11 -0500

    Added CORS using flask_cors

--- connectify-service.py
+++ connectify-service.py
@@ -8,12 +8,19 @@ Licensed by Connectify.
 Designed and Coded by Shrikant.
 '''
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+HOST = "0.0.0.0"
+PORT = 3000
+ALLOWED_ORIGINS = "*"
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": ALLOWED_ORIGINS}})
 
-@app.route("/")
+@app.route("/connectifyapi/hello")
+@cross_origin()
 def hello():
     return "Hello User! This is the Connectify backend API for Web, Android and iOS applications. Welcome aboard !"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host=HOST, port=PORT)
